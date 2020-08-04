e104e95bc975282c250da724dc7fb3e387ab878b
mayankbansa0105 <62702672+mayankbansa0105@users.noreply.github.com>
2020-08-04 18:36:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var app = express();
+var http = require("http");
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
-})
-
-app.set('port', process.env.PORT || 3000);
-var server = app.listen(port, function () {
-   var host = server.address().address
-   var port = server.address().port
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
    
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:3000/');
